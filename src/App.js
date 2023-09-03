2147bf7932427033b67be4e92e561fe21942e56b
Zabair Ghoss <czubair307@gmail.com>
2023-09-03 22:02:20 +0500

    Home component added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { ToastContainer } from "react-bootstrap";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <ToastContainer />
       <Navbar />
       <Routes>
+          <Route exact path="/" Component={()=> <Home />}/>
           <Route path="/add" element={<div>This is Add Contact</div>} />
           <Route path="/update/:id" element={<div>This is Update Contact</div>} />
           </Routes>
