90cd38747fe6aaf153b2cc8fb41b2187ffe959b2
Zabair Ghoss <czubair307@gmail.com>
2023-09-03 12:42:05 +0500

    Initialized App dependencies and initial app structure set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import { ToastContainer } from "react-bootstrap";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-        <h1>Welcome to React Redux Contact App</h1>      
+      <ToastContainer />
+      <h1>Welcome to React Redux Contact App</h1>
     </div>
   );
-}
+};
 
 export default App;
